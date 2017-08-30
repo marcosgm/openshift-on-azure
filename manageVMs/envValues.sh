fa275b8502a7885ffbe4d9b4bd293897d5c4405d
Marcos Garcia <mgarciam@redhat.com>
2017-08-30 16:51:47 -0400

    marcos' personal envValues

---
+++ manageVMs/envValues.sh
@@ -0,0 +1,14 @@
+location="westus2"
+resourceGroupName="openshift-workshop"
+vnetName="openshift-workshop-westus2-vnet"
+subnetName="default"
+subnetAddressPrefix="10.0.0.0/24"
+networkSecurityGroup="master-workshop-westus2"
+storageAccountName="marcoswkshpwestus2"
+adminUserName="marcos"
+
+publicIPName="marcosJumpServerIP"
+nicName="marcosJumpServerNIC"
+vmName="marcos-jump-server"
+vmSize="Standard_DS1_V2"
+
